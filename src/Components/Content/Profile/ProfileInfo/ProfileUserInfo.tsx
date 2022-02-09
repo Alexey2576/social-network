ecfd9439ecc4739405eb35c8baac6ca3c225ee06
Alexey2576 <alexeynikinitin@yandex.by>
2022-02-10 00:26:25 +0300

    Create ErrorBoundary and use him with Profile children components

--- src/Components/Content/Profile/ProfileInfo/ProfileUserInfo.tsx
+++ src/Components/Content/Profile/ProfileInfo/ProfileUserInfo.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import s from "../profile.module.scss";
 import {Status, StatusType} from "./Status/Status";
 import {ProfileUserInfoType} from "../../../../redux/profile-redux/profileReducer";
+import {ErrorBoundary} from "../../../Commons/ErrorBoundary/ErrorBoundary";
 
 export const ProfileUserInfo = React.memo((props: ProfileUserInfoType & StatusType) => {
-   const { photos, fullName, contacts, userID, ...statusProps } = props
+   const {photos, fullName, contacts, userID, ...statusProps} = props
    return (
-      <div className={s.profile__info}>
-         <div className={s.profile__info_img}>
-            <img src={photos.large} alt=""/>
+      <ErrorBoundary>
+         <div className={s.profile__info}>
+            <div className={s.profile__info_img}>
+               <img src={photos.large} alt=""/>
+            </div>
+            <div className={s.profile__info_text}>
+               <h3>{fullName}</h3>
+               <Status {...statusProps} userID={userID}/>
+               <h4>{contacts.github}</h4>
+            </div>
          </div>
-         <div className={s.profile__info_text}>
-            <h3>{fullName}</h3>
-            <Status {...statusProps} userID={userID}/>
-            <h4>{contacts.github}</h4>
-         </div>
-      </div>
+      </ErrorBoundary>
    )
 })
